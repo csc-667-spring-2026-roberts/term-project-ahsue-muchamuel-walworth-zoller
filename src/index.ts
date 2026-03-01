f7001d528a2901c7631030850ca9bb7904b3e8de
Sophia <szoller@sfsu.edu>
2026-02-28 20:44:57 -0800

    server set up

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import homeRoutes from "./routes/home.js";
+import loggingMiddleware from "./middleware/logging.js";
+
+const app = express();
+const PORT = process.env.PORT ?parseInt(process.env.PORT) : 3000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+//--- Middleware ---
+app.use(express.json()); // parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // parse form data
+app.use(express.static(path.join(__dirname, "..", "public"))); // serve static files
+// --- Logging (we'll extract this to middleware/ later) ---
+app.use(loggingMiddleware);
+app.use("/", homeRoutes);
+// --- Routes ---
+
+// --- Start ---
+app.listen(PORT, () => {
+console.log(`Server running on http:localhost:${PORT}`);
+});
---
+++ src/routes/home.ts
@@ -0,0 +1,9 @@
+import {Router } from "express";
+
+const router = Router();
+
+router.get("/", (_request, response) => {
+response.send("<h1>It works!</h1>");
+});
+
+export default router;
